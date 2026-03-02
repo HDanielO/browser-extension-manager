0e89784722b32251b559038adef6682da50afd94
HDanielO <hameeddnl@gmail.com>
2026-03-02 03:55:21 +0100

    Enhance styling and layout: add color variables, update NavBar background, and improve dashboard tab design

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,20 @@ import NavBar from "@/components/NavBar/navbar.js";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-(--neutral-0)">
       <main className="flex-1">
         <NavBar></NavBar>
+        <div className="dashboardTab w-9/10 my-0.5 mx-auto flex flex-col items-center">
+          <h2
+            className="tabHeader text-2xl font-bold 
+          text-(--neutral-800)"
+          >
+            Extension List
+          </h2>
+        </div>
       </main>
 
-      <div className="flex justify-center text-center text-sm pb-4 text-gray-600">
+      <div className="flex justify-center text-center text-xs pb-4 text-gray-600">
         Challenge by{"  "}
         <a
           href="https://www.frontendmentor.io?ref=challenge"
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -3,14 +3,14 @@ import Image from "next/image";
 const NavBar = () => {
   return (
     <div className="nav">
-      <div className="navLayout flex justify-between items-center w-9/10 my-4 mx-auto px-3 py-2 rounded-xl shadow cursor-pointer max-w-250">
+      <div className="navLayout flex justify-between items-center w-9/10 my-4 mx-auto px-3 py-2 rounded-xl shadow cursor-pointer max-w-250 bg-white">
         <Image
           src="/images/logo.svg"
           alt="ExtensionHub Logo"
-          width="160"
+          width="150"
           height="10"
         ></Image>
-        <span className="w-9 flex justify-center items-center rounded-md bg-gray-200 self-stretch cursor-pointer">
+        <span className="w-9 flex justify-center items-center rounded-md bg-(--neutral-100) self-stretch cursor-pointer">
           <Image
             src="/images/icon-moon.svg"
             alt="ExtensionHub Logo"
